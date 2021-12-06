602765e2299b1d1721f388c34193a7cf22fb5bb6
GorgonUK <georgekontus@gmail.com>
2021-12-06 16:25:05 +0000

    SQL Setup

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Order_Genie.Models;
+
+namespace Order_Genie.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+public virtual DbSet<User> Users { get; set; }
+        
+        protected virtual void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseMySql("server=127.0.0.1;port=3306;user=root;password=root;database=ordergenie")
+                .UseLoggerFactory(LoggerFactory.Create(b => b
+                    .AddConsole()
+                    .AddFilter(level => level >= LogLevel.Information)))
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+            
+        }
+    }
+}
